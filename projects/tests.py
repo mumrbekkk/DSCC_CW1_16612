44435d6446c37c7a627da2415201773d61d8d68a
Umrbek <madatovumrbek8@gmail.com>
2026-03-01 16:24:09 +0500

    Added CI pipeline

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Project
 
-# Create your tests here.
+User = get_user_model()
+
+class ProjectModelTest(TestCase):
+
+    def test_project_creation(self):
+        user = User.objects.create_user(username="testuser", password="123")
+        project = Project.objects.create(name="Test", owner=user)
+        self.assertEqual(project.name, "Test")
